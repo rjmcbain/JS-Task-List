9a9cb8aeb7634862b1c14d305be00addb0de15c0
rjmcbain <rmcbain01@gmail.com>
2018-06-28 17:26:41 -0600

    adding event listeners

--- main.js
+++ main.js
@@ -4,3 +4,40 @@ const taskList = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener("submit", addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  // Create li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = "collection-item";
+  // Create text node and appent to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class
+  link.className = "delete-item secondary-content";
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  //Append the link to le
+  li.appendChild(link);
+
+  // Append li to ul
+
+  console.log(li);
+
+  e.preventDefault();
+}
