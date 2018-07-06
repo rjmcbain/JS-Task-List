1af6e236ef0a380eb8b232a94389434aaa3b3448
rjmcbain <rmcbain01@gmail.com>
2018-07-06 17:55:43 -0600

    adding filter function

--- main.js
+++ main.js
@@ -72,6 +72,14 @@ function clearTasks() {
 
 // Filter Tasks
 function filterTasks(e) {
-  const text = e.target.value;
-  
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
