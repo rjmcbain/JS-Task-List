a403fc73de496416b5e9124feb2e4e6515bf8929
rjmcbain <rmcbain01@gmail.com>
2018-07-06 18:19:25 -0600

    adding local storage

--- main.js
+++ main.js
@@ -30,6 +30,26 @@ function getTasks() {
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
+
+  tasks.forEach(function(task) {
+     // Create li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = "collection-item";
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(task));
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class
+  link.className = "delete-item secondary-content";
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+  })
 }
 
 // Add Task
